4ad1a483fb90d4cb72dfa9842a723e9899c1ab7b
Manish Bhattarai <ceodspspectrum@pn2001146.lanl.gov>
2021-04-06 23:42:36 -0600

    edits

--- pynmfk/config.py
+++
@@ -1,5 +0,0 @@
-def init(arg):
-    """ Global variables declaration here. The variables declared within this function in this file are shared across other files and functions during import."""
-    global time, flag
-    time = {}
-    flag = 0
